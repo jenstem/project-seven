96b9694fab3c54ae9b0f7a8324d5dbf6d08d39e2
Jen <jennifer.stemkowski@gmail.com>
2024-03-28 15:25:59 -0400

    Create delete btn + undo func

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import ast
 
 root =Tk()
 
@@ -17,6 +18,27 @@ def get_operation(operator):
 
 def clear_all():
     display.delete(0, END)
+
+def calculate():
+    entire_string = display.get()
+    try:
+        node = ast.parse(entire_string, mode='eval')
+        result = eval(compile(node, '<string>', 'eval'))
+        clear_all()
+        display.insert(0, result)
+    except Exception:
+        clear_all()
+        display.insert(0, "Error")
+
+def undo():
+    entire_string = display.get()
+    if len(entire_string):
+        new_string = entire_string[:-1]
+        clear_all()
+        display.insert(0, new_string)
+    else:
+        clear_all()
+        display.insert(0, "")
 
 display = Entry(root)
 display.grid(row=1, columnspan=6)
@@ -53,6 +75,9 @@ for x in range(4):
 Button(root, text="AC", width=3, height=3, command=clear_all).grid(row=5, column=0)
 
 # Equal button
-Button(root, text="=", width=3, height=3).grid(row=5, column=2)
+Button(root, text="=", width=3, height=3, command=calculate).grid(row=5, column=2)
+
+# Delete button
+Button(root, text="<-", width=3, height=3, command=lambda : undo()).grid(row=5, column=4)
 
 root.mainloop()
